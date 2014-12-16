20d6c0603923f98ab261ddb1dfd78a615d9f9b00
alessandrorimoldi91 <alessandrorimoldi91@gmail.com>
2014-12-16 15:10:20 +0000

    aggiunte le entities da database

---
+++ MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/entities/Calendar.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Alessandro
+ */
+@Entity
+@Table(name = "calendar")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Calendar.findAll", query = "SELECT c FROM Calendar c"),
+    @NamedQuery(name = "Calendar.findById", query = "SELECT c FROM Calendar c WHERE c.id = :id"),
+    @NamedQuery(name = "Calendar.findByPrivate1", query = "SELECT c FROM Calendar c WHERE c.private1 = :private1")})
+public class Calendar implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "id")
+    private String id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "private")
+    private boolean private1;
+    @JoinTable(name = "calendar_composition", joinColumns = {
+        @JoinColumn(name = "calendar_id", referencedColumnName = "id")}, inverseJoinColumns = {
+        @JoinColumn(name = "event_id", referencedColumnName = "id")})
+    @ManyToMany
+    private Collection<Event> eventCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "calendarId")
+    private Collection<User> userCollection;
+    @JoinColumn(name = "user", referencedColumnName = "email")
+    @ManyToOne(optional = false)
+    private User user;
+
+    public Calendar() {
+    }
+
+    public Calendar(String id) {
+        this.id = id;
+    }
+
+    public Calendar(String id, boolean private1) {
+        this.id = id;
+        this.private1 = private1;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean getPrivate1() {
+        return private1;
+    }
+
+    public void setPrivate1(boolean private1) {
+        this.private1 = private1;
+    }
+
+    @XmlTransient
+    public Collection<Event> getEventCollection() {
+        return eventCollection;
+    }
+
+    public void setEventCollection(Collection<Event> eventCollection) {
+        this.eventCollection = eventCollection;
+    }
+
+    @XmlTransient
+    public Collection<User> getUserCollection() {
+        return userCollection;
+    }
+
+    public void setUserCollection(Collection<User> userCollection) {
+        this.userCollection = userCollection;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Calendar)) {
+            return false;
+        }
+        Calendar other = (Calendar) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.meteocal.entities.Calendar[ id=" + id + " ]";
+    }
+    
+}
---
+++ MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/entities/Notification.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author Alessandro
+ */
+@Entity
+@Table(name = "notification")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Notification.findAll", query = "SELECT n FROM Notification n"),
+    @NamedQuery(name = "Notification.findById", query = "SELECT n FROM Notification n WHERE n.id = :id"),
+    @NamedQuery(name = "Notification.findByTimestamp", query = "SELECT n FROM Notification n WHERE n.timestamp = :timestamp"),
+    @NamedQuery(name = "Notification.findByType", query = "SELECT n FROM Notification n WHERE n.type = :type")})
+public class Notification implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 10)
+    @Column(name = "id")
+    private String id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+    @Lob
+    @Size(max = 16777215)
+    @Column(name = "text")
+    private String text;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "type")
+    private String type;
+    @ManyToMany(mappedBy = "notificationCollection")
+    private Collection<User> userCollection;
+    @JoinColumn(name = "sender", referencedColumnName = "email")
+    @ManyToOne
+    private User sender;
+    @JoinColumn(name = "event_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Event eventId;
+
+    public Notification() {
+    }
+
+    public Notification(String id) {
+        this.id = id;
+    }
+
+    public Notification(String id, Date timestamp, String type) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @XmlTransient
+    public Collection<User> getUserCollection() {
+        return userCollection;
+    }
+
+    public void setUserCollection(Collection<User> userCollection) {
+        this.userCollection = userCollection;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public Event getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Event eventId) {
+        this.eventId = eventId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Notification)) {
+            return false;
+        }
+        Notification other = (Notification) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.polimi.meteocal.entities.Notification[ id=" + id + " ]";
+    }
+    
+}
