3bf304cd3d8a22fa1914577a3810fa174f5bd633
Francesco.Lattari92@gmail.com <Francesco.Lattari92@gmail.com@3b6f2f1b-23b6-aa71-39df-c8371fa46846>
2014-12-23 14:00:09 +0000

    Login and registration

---
+++ trunk/MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/RegistrationBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.gui.security;
+
+import it.polimi.meteocal.business.security.boundary.UserManager;
+import it.polimi.meteocal.business.security.entity.User;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author miglie
+ */
+@Named
+@RequestScoped
+public class RegistrationBean {
+
+    @EJB
+    private UserManager um;
+
+    private User user;
+
+    public RegistrationBean() {
+    }
+
+    public User getUser() {
+        if (user == null) {
+            user = new User();
+        }
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String register() {
+        um.save(user);
+        return "user/user_home?faces-redirect=true";
+    }
+
+}
