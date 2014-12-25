82b280393c85a7d21bf93d48b3c8b8533651e5ce
alessandrorimoldi91@gmail.com <alessandrorimoldi91@gmail.com@3b6f2f1b-23b6-aa71-39df-c8371fa46846>
2014-12-25 22:03:37 +0000

    Aggiunta la user home. Attualmente non funziona a causa di questo comando in user_home.xhtml:
    <p:graphicImage value="#{ub.getUserAvatar()}" />
    
    Il comando serve a caricare l'immagine profilo dell'utente, ma per qualche motivo fa bloccare l'applicazione.
    Già consultata la documentazione di primefaces senza risultato: http://www.primefaces.org/docs/guide/primefaces_user_guide_5_1.pdf (pag 231)
    NB: attualmente l'immagine profilo "icon-user-default.png" è caricata tramite path assoluto valido solo per il mio pc.

--- trunk/MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/UserBean.java
+++ trunk/MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/UserBean.java
@@ -7,10 +7,16 @@ package it.polimi.meteocal.gui.security;
 
 import it.polimi.meteocal.business.security.boundary.UserManager;
 import it.polimi.meteocal.business.security.entity.User;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 /**
  *
@@ -28,6 +34,36 @@ public class UserBean{
     public UserBean() {
     }
     
+    public StreamedContent getUserAvatar() {
+//        byte[] buffer;
+//        FacesContext fc = FacesContext.getCurrentInstance();
+//        if (fc.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+//            return new DefaultStreamedContent();
+//        } else {
+//            buffer = getCurrentUser().getAvatar();
+//            InputStream input = new ByteArrayInputStream(buffer);
+//            StreamedContent image = new DefaultStreamedContent(input, "image/png");
+//            return image;
+//        }
+        InputStream is = new ByteArrayInputStream(getCurrentUser().getAvatar());
+        StreamedContent image = new DefaultStreamedContent(is, "image/png");
+        return image;
+    }
+    
+    public String getPhoneNumber() {
+        if (getCurrentUser().getPhone() == null) {
+            return "not available";
+        }
+        return getCurrentUser().getPhone().toString();
+    }
+    
+    public String getCalendarPrivacy() {
+        if (getCurrentUser().getPrivacy()) {
+            return "private";
+        }
+        return "public";
+    }
+    
     public String getName() {
         return um.getLoggedUser().getName();
     }
@@ -43,11 +79,11 @@ public class UserBean{
         return newUser;
     }
      
-    public void create() {
+    public void create() throws IOException {
          um.save(newUser);
     }
     
-    public User getActualLoggedUser(){
+    public User getCurrentUser(){
         return um.getLoggedUser();
     }
     
