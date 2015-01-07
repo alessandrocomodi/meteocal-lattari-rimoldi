46f9a81c123912283e7d4bd3c332f6bb4a990ac8
alessandrorimoldi91 <alessandrorimoldi91@gmail.com>
2015-01-07 09:02:59 +0000

    modifica provvisoria alla ricerca utenti

--- MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/business/security/boundary/SearchManager.java
+++ MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/business/security/boundary/SearchManager.java
@@ -18,26 +18,28 @@ public class SearchManager {
     @PersistenceContext
     private EntityManager em;
 
-    public ArrayList<User> searchUsers(String parameter) {
+    public List<String> searchUsers(String parameter) {
+        String param = "Ciccio";
+        parameter = param;
         Query email= em.createNamedQuery("User.findByEmail");
-        email.setParameter("email", parameter);
+        email.setParameter("email", param);
         Query name= em.createNamedQuery("User.findByName");
-        name.setParameter("name", parameter);
+        name.setParameter("name", param);
         Query surname= em.createNamedQuery("User.findBySurname");
-        surname.setParameter("surname", parameter);
+        surname.setParameter("surname", param);
         Query phone= em.createNamedQuery("User.findByPhone");
-        phone.setParameter("phone", parameter);
-        ArrayList results= new ArrayList<String>();
+        phone.setParameter("phone", param);
+        List<String> results = new ArrayList<>();
         results.addAll(email.getResultList());
         results.addAll(name.getResultList());
         results.addAll(surname.getResultList());
         results.addAll(phone.getResultList());
-        ArrayList users= new ArrayList<User>();
-        for(int i=0; i<results.size(); i++){
-            User u = em.find(User.class, results.get(i));
-            users.add(u);
-        }
-        return users;
+//        ArrayList<User> users = new ArrayList<>();
+//        for(int i=0; i<results.size(); i++){
+//            User u = em.find(User.class, results.get(i));
+//            users.add(u);
+//        }
+        return results;
     }
 
 }
