eae8265c3375db5c9904a88d22772cb727ad8506
alessandrorimoldi91 <alessandrorimoldi91@gmail.com>
2015-01-15 16:01:04 +0000

    possibilità di accettare l'evento

--- MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/business/security/entity/User.java
+++ MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/business/security/entity/User.java
@@ -187,7 +187,7 @@ public class User implements Serializable {
     }
 
     @XmlTransient
-    public Collection<Event> getEventCollection() {
+    public List<Event> getEventCollection() {
         return eventCollection;
     }
 
