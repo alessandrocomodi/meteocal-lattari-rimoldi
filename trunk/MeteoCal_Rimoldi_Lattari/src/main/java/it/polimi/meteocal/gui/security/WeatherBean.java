b37d2133cc4166171dd33b03e051f120165724da
alessandrorimoldi91@gmail.com <alessandrorimoldi91@gmail.com@3b6f2f1b-23b6-aa71-39df-c8371fa46846>
2015-01-20 10:40:49 +0000

    piccola modifica ai risultati listati

--- trunk/MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/WeatherBean.java
+++ trunk/MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/WeatherBean.java
@@ -36,15 +36,7 @@ public class WeatherBean {
     private String lon2;
     
     
-    private List<String> weatherConditions;
-
-    public List<String> getWeatherConditions() {
-        return weatherConditions;
-    }
-
-    public void setWeatherConditions(List<String> weatherConditions) {
-        this.weatherConditions = weatherConditions;
-    }
+    
 
     public String getLon2() {
         return lon2;
@@ -110,70 +102,6 @@ public class WeatherBean {
     public WeatherBean() {
     }
     
-    public void retrieveConditions() throws ParserConfigurationException, MalformedURLException, SAXException, IOException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-
-        URL oracle2 = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat2 + "&lon=" + lon2 + "&mode=xml&units=metric&cnt=16&APPID=717e3c2ca6c7dfd9a18b4b25d27555af");
-
-        Document doc = db.parse(oracle2.openStream());
-
-        doc.getDocumentElement().normalize();
-
-        //liste di nodi contenenti gli elementi di interesse
-        NodeList dataNodeList = doc.getElementsByTagName("time");
-        NodeList weatherNodeList = doc.getElementsByTagName("symbol");
-        NodeList tempNodeList = doc.getElementsByTagName("temperature");
-        
-        //Lista che conterrà tutti i dati, per ogni cella ci sarà una sequenza day code name min max separati da $
-        List<String> weatherForecast = new ArrayList();
-        weatherConditions = new ArrayList();
-        String day = "";
-        String code = "";
-        String name = "";
-        String min = "";
-        String max = "";
-        
-        //tanto la lunghezza delle liste di nodi è sempre la stessa: 16
-        for (int i = 0; i < dataNodeList.getLength(); i++) {
-            
-            
-            //prima la data
-            Node nNode1 = dataNodeList.item(i);
-            if (nNode1.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) nNode1;
-                //salvo il dato
-                day = element.getAttribute("day");
-            }
-            
-            //poi il meteo
-            Node nNode2 = weatherNodeList.item(i);
-            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) nNode2;
-                //salvo i dati
-                code = element.getAttribute("number");
-                name = element.getAttribute("name");
-            }
-            
-            //infine la temperatura
-            Node nNode3 = tempNodeList.item(i);
-            
-            if (nNode3.getNodeType() == Node.ELEMENT_NODE) {
-                Element element = (Element) nNode3;
-                //salvo i dati
-                min = element.getAttribute("min");
-                max = element.getAttribute("max");
-            }
-            
-            //lista dei dati
-            weatherForecast.add(day + "$" + code + "$" + name + "$" + min + "$" + max);
-            weatherConditions.add("Day: " + day + " Cond: " + name + " Min: "+  min + " Max: " + max);
-        }
-        
-        //prova di output
-        for (String s : weatherForecast) {
-            System.out.println(s);
-        }
-    }
+    
     
 }
