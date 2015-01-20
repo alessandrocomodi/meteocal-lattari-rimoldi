c2fbcf00b6ae2f8c50a58eb448ecdab977432966
alessandrorimoldi91 <alessandrorimoldi91@gmail.com>
2015-01-20 12:07:32 +0000

    aggiunti al db latitudine e longitudine

--- MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/EventBean.java
+++ MeteoCal_Rimoldi_Lattari/src/main/java/it/polimi/meteocal/gui/security/EventBean.java
@@ -115,6 +115,7 @@ public class EventBean {
     
     public String createEvent(User user, List<User> invitedUsers) throws ParserConfigurationException, SAXException, IOException {
         Integer id = 0;
+        
         System.out.println(this.getWeatherConditionsForEventDay());
         em.createEvent(event, user);
         List<Event> result = em.getEventOrganized(user.getEmail());
@@ -152,10 +153,10 @@ public class EventBean {
         this.guests.add(user);
     }
     
-    private String getWeatherConditionsForEventDay() {
+    private String getWeatherConditionsForEventDay() throws ParserConfigurationException, SAXException, IOException {
         String[] parts = new String[5];
         String eventDate = this.getFormattedTime();
-        for (String s : weatherForecast) {
+        for (String s : this.retriveWeatherForecast(event.getLat(), event.getLon())) {
             if (s.contains(eventDate)) {
                 parts = s.split("$");
             } else {
@@ -253,6 +254,68 @@ public class EventBean {
             System.out.println(s);
         }
     }
+    
+    private List<String> retriveWeatherForecast(String lat, String lon) throws ParserConfigurationException, MalformedURLException, SAXException, IOException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+
+        URL oracle2 = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&mode=xml&units=metric&cnt=16&APPID=717e3c2ca6c7dfd9a18b4b25d27555af");
+
+        Document doc = db.parse(oracle2.openStream());
+
+        doc.getDocumentElement().normalize();
+
+        //liste di nodi contenenti gli elementi di interesse
+        NodeList dataNodeList = doc.getElementsByTagName("time");
+        NodeList weatherNodeList = doc.getElementsByTagName("symbol");
+        NodeList tempNodeList = doc.getElementsByTagName("temperature");
+        
+        //Lista che conterrà tutti i dati, per ogni cella ci sarà una sequenza day code name min max separati da $
+        List<String> weatherForecast2 = new ArrayList();
+        String day = "";
+        String code = "";
+        String name = "";
+        String min = "";
+        String max = "";
+        
+        //tanto la lunghezza delle liste di nodi è sempre la stessa: 16
+        for (int i = 0; i < dataNodeList.getLength(); i++) {
+            
+            
+            //prima la data
+            Node nNode1 = dataNodeList.item(i);
+            if (nNode1.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode1;
+                //salvo il dato
+                day = element.getAttribute("day");
+            }
+            
+            //poi il meteo
+            Node nNode2 = weatherNodeList.item(i);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode2;
+                //salvo i dati
+                code = element.getAttribute("number");
+                name = element.getAttribute("name");
+            }
+            
+            //infine la temperatura
+            Node nNode3 = tempNodeList.item(i);
+            
+            if (nNode3.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) nNode3;
+                //salvo i dati
+                min = element.getAttribute("min");
+                max = element.getAttribute("max");
+            }
+            
+            //lista dei dati
+            weatherForecast2.add(day + "$" + code + "$" + name + "$" + min + "$" + max);
+        }
+        return weatherForecast2;
+        
+    }
+        
     
     
     
